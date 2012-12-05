f63a4647982abeecc78e3e9dfe442e2a28fef838
Andrews Medina <andrewsmedina@gmail.com>
2012-12-05 00:06:57 -0200

    added data model

--- metrics/models.py
+++ metrics/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Metric(models.Model):
     name = models.CharField(max_length=255)
     url = models.URLField()
+
+
+class Data(models.Model):
+    metric = models.ForeignKey(Metric)
+    count = models.IntegerField()
+    date = models.DateTimeField()
---
+++ metrics/tests/test_data_model.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from metrics.models import Data
+
+
+class DataModelTestCase(TestCase):
+    def test_should_have_count_field(self):
+        fields = Data._meta.get_all_field_names()
+        self.assertIn("count", fields)
+
+    def test_should_have_date_field(self):
+        fields = Data._meta.get_all_field_names()
+        self.assertIn("date", fields)
+
+    def test_should_have_metric_field(self):
+        fields = Data._meta.get_all_field_names()
+        self.assertIn("metric", fields)
