f41b636fd55e9656d248f922a658e8fee6820a49
Andrews Medina <andrewsmedina@gmail.com>
2014-03-28 00:28:55 -0300

    added github metric.

--- metrics/models.py
+++ metrics/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from datetime import datetime
 
+import requests
+
 
 class Data(models.Model):
     count = models.IntegerField()
@@ -14,3 +16,22 @@ class Metric(models.Model):
 
     class Meta:
         abstract = True
+
+
+class GithubItem(Metric):
+    """Example: https://api.github.com/repos/django/django/pulls?state=open"""
+    url = models.URLField(max_length=1000)
+
+    def fetch(self):
+        count = 0
+        page = 1
+        while True:
+            r = requests.get(self.api_url, params={
+                'page': page, 'per_page': 100
+            })
+            c = len(r.json)
+            count += c
+            page += 1
+            if c < 100:
+                break
+        return count
